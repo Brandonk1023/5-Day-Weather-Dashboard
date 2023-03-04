c2828bf74b45e31edfb1d0e691b649f28b67f5c3
Brandonk1023 <brandonk1023@gmail.com>
2023-03-04 18:45:13 -0500

    corrected API calls so that data is now pulling

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,52 @@
+
+document.getElementById("searchbtn").addEventListener("click", getWeather)
 let apiKey = "e917200951340ec1a63349a80625798c"
-let geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey
-let weatherApi = "http://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + apiKey
-let city = document.getElementById("cityName")
-let cityName = document.getElementById("citySearchInput").value
-let cityLat = y
-let cityLon = x
+let cityLat = ""
+let cityLon = ""
+
 //5 day forecast URL
 //http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //GEO URL
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+function getWeather() {
+    let cityName = document.getElementById("citySearchInput").value
+    let geoApi = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey
 
-fetch(geoApi)
-    .then(function (response) {
-        return response.json()
-    })
+    fetch(geoApi)
+        .then(function (response) {
+            return response.json()
+        })
 
-    .then(function (data) {
-        cityLat = data[0].lat
-        cityLon = data[0].lon
-    })
+        .then(function (data) {
+            console.log(data)
+            cityLat = data[0].lat
+            console.log(cityLat, "CITY LAT VALUE")
+            cityLon = data[0].lon
+            console.log(cityLon, "CITY LON VALUE")
+            
+            let weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + apiKey
+
+            fetch(weatherApi)
+            .then(function (response) {
+                return response.json()
+            })
     
-fetch(weatherApi)
-    .then(function (response) {
-        return response.json()
-    })
+            .then(function (data) {
+    
+            })
+
+        })
 
 
+    for (i = 0; i < 5; i++) {
 
-for (i = 0; i < 5; i++) {
+    }
 
+    function addSearchHistory() {
+
+        document.createElement("button")
+        document.getElementById("searchHistory").appendChild("button")
+
+    }
 }
